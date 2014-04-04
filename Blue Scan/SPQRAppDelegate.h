d50b16b06e8827afbe382951c993fc03f90a429e
Matt Schulte <schultetwin1@gmail.com>
2014-04-04 01:27:35 -0400

    Initial Commit

---
+++ Blue Scan/SPQRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPQRAppDelegate.h
+//  Blue Scan
+//
+//  Created by Matt Schulte on 4/4/14.
+//  Copyright (c) 2014 SPQR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPQRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blue ScanTests/Blue_ScanTests.m
@@ -0,0 +1,34 @@
+//
+//  Blue_ScanTests.m
+//  Blue ScanTests
+//
+//  Created by Matt Schulte on 4/4/14.
+//  Copyright (c) 2014 SPQR. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Blue_ScanTests : XCTestCase
+
+@end
+
+@implementation Blue_ScanTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
