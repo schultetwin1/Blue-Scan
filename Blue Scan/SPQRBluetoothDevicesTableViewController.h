46b7fcf3fa66df1b9c537f8438704ef06611fde4
Matt Schulte <schultetwin1@gmail.com>
2014-04-24 13:11:26 -0400

    Final Version for demo

---
+++ Blue Scan/SPQRBluetoothDevicesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPQRBluetoothDevicesTableViewController.h
+//  Blue Scan
+//
+//  Created by Matt Schulte on 4/4/14.
+//  Copyright (c) 2014 SPQR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPQRBluetoothDevicesTableViewController : UITableViewController <CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@end
---
+++ Blue Scan/SPQRBluetoothDevicesTableViewController.m
@@ -0,0 +1,147 @@
+//
+//  SPQRBluetoothDevicesTableViewController.m
+//  Blue Scan
+//
+//  Created by Matt Schulte on 4/4/14.
+//  Copyright (c) 2014 SPQR. All rights reserved.
+//
+#import <CoreBluetooth/CoreBluetooth.h>
+
+#import "SPQRBluetoothDevicesTableViewController.h"
+
+@interface SPQRBluetoothDevicesTableViewController ()
+
+@property CBCentralManager *myCentralManager;
+@property CBPeripheralManager *peripheral;
+@property BOOL poweredon;
+
+@end
+
+@implementation SPQRBluetoothDevicesTableViewController
+
+-(void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    if (central != self.myCentralManager) {
+        NSLog(@"Different than expected");
+    }
+    
+    if (central.state == CBCentralManagerStatePoweredOn) {
+        self.poweredon = YES;
+        NSLog(@"BLE scanning");
+        [self.myCentralManager scanForPeripheralsWithServices:nil options:nil];
+    } else {
+        NSLog(@"BLE not supported");
+    }
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
+    NSLog(@"Discovered %@", peripheral.name);
+}
+
+
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    self.myCentralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+    self.poweredon = NO;
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
